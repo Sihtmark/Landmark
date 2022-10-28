995d37935d5aa3447c664c8952ff365e1aab04e4
Sergei Poluboiarinov <sihtmark@icloud.com>
2022-10-28 12:57:16 +0300

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by Sergei Poluboiarinov on 28/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
