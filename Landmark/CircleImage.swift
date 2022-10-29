33bc6492602f5f84c2d85764c05848d9f0ab868d
Sergei Poluboiarinov <sihtmark@icloud.com>
2022-10-29 14:28:03 +0300

    feat: Create a Custom Image View

---
+++ Landmark/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Landmark
+//
+//  Created by Sergei Poluboiarinov on 29/10/2022.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+                    .shadow(radius: 7)
+            }
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
