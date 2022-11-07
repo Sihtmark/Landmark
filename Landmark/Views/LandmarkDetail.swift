59deda14492bc7c272327f384d572740c523c429
Sergei Poluboiarinov <sihtmark@icloud.com>
2022-11-07 12:46:53 +0300

    feat: Adopt the Model Object in Your Views

--- Landmark/Views/LandmarkDetail.swift
+++ Landmark/Views/LandmarkDetail.swift
@@ -9,21 +9,21 @@ import SwiftUI
 
 struct LandmarkDetail: View {
     var landmark: Landmark
-    
+
     var body: some View {
         ScrollView {
             MapView(coordinate: landmark.locationCoordinate)
                 .ignoresSafeArea(edges: .top)
                 .frame(height: 300)
-            
+
             CircleImage(image: landmark.image)
                 .offset(y: -130)
                 .padding(.bottom, -130)
-            
+
             VStack(alignment: .leading) {
                 Text(landmark.name)
                     .font(.title)
-                
+
                 HStack {
                     Text(landmark.park)
                     Spacer()
@@ -31,9 +31,9 @@ struct LandmarkDetail: View {
                 }
                 .font(.subheadline)
                 .foregroundColor(.secondary)
-                
+
                 Divider()
-                
+
                 Text("About \(landmark.name)")
                     .font(.title2)
                 Text(landmark.description)
@@ -47,6 +47,6 @@ struct LandmarkDetail: View {
 
 struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkDetail(landmark: landmarks[0])
+        LandmarkDetail(landmark: ModelData().landmarks[0])
     }
 }
--- Landmark/Views/LandmarkRow.swift
+++ Landmark/Views/LandmarkRow.swift
@@ -8,20 +8,21 @@
 import SwiftUI
 
 struct LandmarkRow: View {
-    
     var landmark: Landmark
-    
+
     var body: some View {
         HStack {
             landmark.image
                 .resizable()
                 .frame(width: 50, height: 50)
+                .cornerRadius(5)
             Text(landmark.name)
-            
+
             Spacer()
-            
+
             if landmark.isFavorite {
                 Image(systemName: "star.fill")
+                    .imageScale(.medium)
                     .foregroundColor(.yellow)
             }
         }
@@ -29,6 +30,8 @@ struct LandmarkRow: View {
 }
 
 struct LandmarkRow_Previews: PreviewProvider {
+    static var landmarks = ModelData().landmarks
+
     static var previews: some View {
         Group {
             LandmarkRow(landmark: landmarks[0])
